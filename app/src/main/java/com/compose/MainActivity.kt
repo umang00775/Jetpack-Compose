ee5f1e71f6b26036225a1069254fc1c99a57d4a3
Umang Rathod <umang00775@gmail.com>
2023-11-01 02:06:37 +0530

    Alignment + Arrangement completed

--- app/src/main/java/com/compose/MainActivity.kt
+++ app/src/main/java/com/compose/MainActivity.kt
@@ -7,7 +7,9 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -22,7 +24,8 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth(1f)
                     .background(Color.Green),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
