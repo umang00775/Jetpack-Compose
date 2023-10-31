333350f4de34d0339498835e79f0f43411c35570
Umang Rathod <umang00775@gmail.com>
2023-11-01 01:39:19 +0530

    Initial commit

---
+++ app/src/main/java/com/compose/MainActivity.kt
@@ -0,0 +1,42 @@
+package com.compose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.compose.ui.theme.ComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color.Green),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(text = "Hello")
+                Text(text = " World!")
+            }
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeTheme {
+    }
+}
