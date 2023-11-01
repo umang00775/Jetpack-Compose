f4b3286cab323348d14a8f9cdd8060bbb8c7312f
Umang Rathod <umang00775@gmail.com>
2023-11-02 01:04:11 +0530

    Cards & Images completed

--- app/src/main/java/com/compose/MainActivity.kt
+++ app/src/main/java/com/compose/MainActivity.kt
@@ -1,47 +1,97 @@
 package com.compose
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Row(modifier = Modifier
-                .fillMaxSize(1f)
-                .background(Color.Magenta),
-                horizontalArrangement = Arrangement.Center,
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Text(text = "Click me", modifier = Modifier
-                    .background(Color.Green)
-                    .alpha(0.7f)
-                    .clickable {
-                        Toast.makeText(this@MainActivity, "You clicked :)" ,Toast.LENGTH_LONG).show()
-                    }
+            val painter = painterResource(id = R.drawable.img)
+            val contentDisc = "He he"
+            val title = "I am the title :)"
+
+            Box(modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .padding(16.dp)
+            ){
+                ImageCard(
+                    painter = painter,
+                    contentDescription = contentDisc,
+                    title = title
                 )
             }
 
         }
     }
 }
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+){
+    Card( modifier = modifier.fillMaxWidth(),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(defaultElevation = 5.dp)
+    ) {
+        Box(modifier = Modifier.height(200.dp)){
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.Crop,
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
+                )
+            )
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(text = title, style = TextStyle(color = Color.White), fontSize = 16.sp)
+            }
+
+        }
+    }
+}
+
 
 @Preview(showBackground = true)
 @Composable
