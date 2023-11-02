4606d9b54a07cddf169f3e76a1f67c1d1eaa430c
Umang Rathod <umang00775@gmail.com>
2023-11-02 23:56:26 +0530

    Styling text completed

--- app/src/main/java/com/compose/MainActivity.kt
+++ app/src/main/java/com/compose/MainActivity.kt
@@ -23,8 +23,17 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontSynthesis.Companion.Style
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,15 +41,56 @@ import androidx.compose.ui.unit.sp
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        val fontFamily = FontFamily(
+            Font(R.font.firasans_regular, FontWeight.Bold),
+            Font(R.font.firasans_regular, FontWeight.Black)
+        )
+
         setContent {
             Box(modifier = Modifier
                 .fillMaxSize()
                 .background(Color(0xFF101010))
             ) {
                 Text(
-                    text = "Learning Jetpack Compose!!",
+                    text = buildAnnotatedString{
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 40.sp
+                            )
+                        ){
+                            append("L")
+                        }
+                        append("earning ")
+
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 40.sp
+                            )
+                        ){
+                            append("J")
+                        }
+                        append("etpack ")
+
+                        withStyle(
+                            style = SpanStyle(
+                                color = Color.Green,
+                                fontSize = 40.sp
+                            )
+                        ){
+                            append("C")
+                        }
+                        append("ompose")
+                    },
+
                     color = Color.White,
-                    fontSize = 30.sp
+                    fontSize = 25.sp,
+                    fontFamily = fontFamily,
+                    fontStyle = FontStyle.Italic,
+                    textAlign = TextAlign.Center,
+                    textDecoration = TextDecoration.Underline
                 )
             }
         }
